c338a10afb9161d2f08efdf64b5d35a126c42d8e
Suki Tsubaki <58035709+sukitsubaki@users.noreply.github.com>
2025-03-19 21:16:16 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pixtrail",
-    version="1.0.0",
+    version="1.0.1",
     author="Suki Tsubaki",
     description="Extract GPS data from photos and create GPX files",
     long_description=long_description,
