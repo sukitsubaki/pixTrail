c4d7d2157aab7ca9b0aac2173e8416097ca449c2
Suki Tsubaki <58035709+sukitsubaki@users.noreply.github.com>
2025-03-23 23:52:46 +0100

    undo for test reasons

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pixtrail",
-    version="2.3.3",
+    version="2.3.2",
     author="Suki Tsubaki",
     description="Extract GPS data from photos and create GPX files",
     long_description=long_description,
